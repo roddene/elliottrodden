1c84021bace9e47c9b3017f5b28a14797130ab98
roddene <44795682+roddene@users.noreply.github.com>
2021-09-12 17:37:13 -0400

    fixed home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import {Fragment} from 'react';
 import Image from 'next/image'
 import NavBar from './components/navbar'
+import profilePic from '../public/photos/IMG_20210906_164421.jpg';
 function HomePage(){
     return(<Fragment>
    <NavBar></NavBar>
     <h1>Home</h1>
-    <Image className = "mainpic" src = "photos/IMG_20210906_164421.jpg"/>
-    <p>Welcome to my Website!</p>
+    <div className = "mainpic">
+    <Image src = {profilePic} alt = "" />
+    </div>
     </Fragment>
 
     )}
