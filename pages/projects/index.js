db73e008e7fab9bb4519d54d78ec4ba72e3e76cb
roddene <44795682+roddene@users.noreply.github.com>
2021-09-21 00:01:23 -0400

    spelling

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -29,7 +29,7 @@ function Projects() {
                 <p className="projdesc">This project uses the YouTube and Spotify APIs in order to play music that exists on Spotify through a YouTube player.
                 Users can sign up to this website and then create as many playlists as they want and add as many songs as they want to each of their playlists.
                 The player can interact with the music by playing, pausing, repeating, skipping, and shuffling within the playlist, or changing the volume up and down.  
-                Users can search Spotify for songs that they want to add to their playlist and a custom database will store their selections for them to come back to.
+                Users can search Spotify for songs that they want to add to their playlist, and then a custom database will store their selections for them to come back to.
 
                 </p>
                 <div className="projectimg">
